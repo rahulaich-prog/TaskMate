35aeee21e7fc69f7d967b07ef010b8d0bca4ef48
Rahul Aich <aichrahul8906@gmail.com>
2025-06-08 12:35:49 +0530

    Initial commit for TaskMate project1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar =()=>{
     return(
        <nav className='flex justify-between bg-violet-900 text-white py-2'>
             <div className='logo'>
-                <span className='font-bold text-xl mx-8'>MateTask</span>
+                <span className='font-bold text-xl mx-8'>TaskMate</span>
             </div>
             <ul className='flex gap-8 mx-9'>
                 <li className='cursor-pointer hover:font-bold tansition-all '>Home</li>
